c9604e74d04c279e8a0fa11a685ffe22136610dd
HibikiHata <htaahbkiii@gmail.com>
2022-03-23 20:20:28 +0900

    ひとまず

--- resources/js/views/mypage/Plan/Plan_edit.vue
+++ resources/js/views/mypage/Plan/Plan_edit.vue
@@ -215,10 +215,12 @@ export default {
     methods: {
         ...mapActions('auth', ['updateState']),
         async Store(){
+            // let url = process.env.MIX_VUE_APP_API_URL + "com/contract/update/do";
             let url = process.env.MIX_VUE_APP_API_URL + "com/company/license";
             try {
                 this.item = {...this.item, company_code: this.$store.state.auth.company.company_code}
                 const response = await axios.post(url, this.item);
+                console.log(response);
                 if(response.data=="NG"){
                     this.message = response.data.message
                     setTimeout(() => {this.message = false;}, 2000);
