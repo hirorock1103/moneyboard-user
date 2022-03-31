8d9058387cc54cbaefdf8dd21cc330fb743af2ec
HibikiHata <htaahbkiii@gmail.com>
2022-03-31 12:09:56 +0900

    追加修正

--- resources/js/views/mypage/Reps/Reps.vue
+++ resources/js/views/mypage/Reps/Reps.vue
@@ -114,6 +114,7 @@ export default {
             try {
                 const response = await axios.get(url);
                 this.items = response.data.data.data_list.data;
+                this.resetTemps();
                 // console.log(this.items);
             } catch (e){
                 console.log(e);
