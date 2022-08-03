378b6c9a1bb669b0cafc772b14fd4748f5f8b64c
HibikiHata <htaahbkiii@gmail.com>
2022-08-03 12:13:27 +0900

    テスト

--- resources/js/store/modules/auth.js
+++ resources/js/store/modules/auth.js
@@ -177,7 +177,7 @@ const actions = {
         // if (response.status === UNPROCESSABLE_ENTITY) {
         if (response.data.status === "NG") {
             // context.commit('setSignupErrorMessages', response.data.message)
-            context.commit('setSignupErrorMessages', '既に登録されているメールアドレスです')
+            context.commit('setSignupErrorMessages', '既に登録されているメールアドレスです\n'.response.data.message)
         } else {
             context.commit('error/setCode', response.status, { root: true })
         }
