9c62f465dd36e7241209541fcbca5184394cc7f7
HibikiHata <htaahbkiii@gmail.com>
2023-05-18 12:57:01 +0900

    確認項目削除し忘れ

--- resources/js/views/mypage/Client/Client.vue
+++ resources/js/views/mypage/Client/Client.vue
@@ -15,7 +15,7 @@
                     <div class="
                     [ display-flex  justify-content-between-large  align-items-baseline  [ flex-column  flex-row-large ] ]  [ padding-left--16  padding-right-16  padding-medium--0 ]  [ [ margin-left-medium--48  margin-left-large--24 ] [ margin-right-medium--48  margin-right-large--24 ] margin-bottom--24 ]  border-bottom">
                         <h2 class="[ margin-bottom--4  margin-bottom-large--16 ]">
-                            登録企業一覧111
+                            登録企業一覧
                         </h2>
                     </div>
                     <form v-on:submit.prevent="clientSearch(1)">
