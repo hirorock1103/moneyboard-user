88cfc387abdc13cb729cbcfd70df8462a60d4d4c
HibikiHata <htaahbkiii@gmail.com>
2023-05-23 18:25:27 +0900

    戻し

--- resources/js/views/mypage/Client/Client.vue
+++ resources/js/views/mypage/Client/Client.vue
@@ -257,9 +257,7 @@ export default {
             }
         },
         async getItem(id, client_name, user_id, client_code) {
-                console.log('getItem');
                 let items = {id:id, client_name:client_name, user_id:user_id, client_code:client_code}
-                console.log(items);
                 this.resetTemps();
                 this.updateTemps(items);
                 this.$router.push({name: 'mypage-client_edit'})
