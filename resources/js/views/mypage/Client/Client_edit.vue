cc1790fcaaed7f10c3d2533b136ef0827469b905
HibikiHata <htaahbkiii@gmail.com>
2023-05-24 17:07:41 +0900

    微修正

--- resources/js/views/mypage/Client/Client_edit.vue
+++ resources/js/views/mypage/Client/Client_edit.vue
@@ -35,7 +35,7 @@
                                                 </th>
                                                 <td class="[ display-table-row  display-table-cell-large ]  padding-bottom--16">
                                                     <select v-model="selectedItem" class="form-input">
-                                                        <option v-for="item in items" :key="item._id" :value="item.id" :selected="item.id === getTemps.user_id">
+                                                        <option v-for="item in items" :key="item._id" :value="item.id" :selected="item.id == getTemps.user_id">
                                                             {{item.user_name}}
                                                         </option>
                                                     </select>
