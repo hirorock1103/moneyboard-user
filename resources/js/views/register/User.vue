557d757fedc0fca2a5c7f59f1ae78f35098d1a8b
HibikiHata <htaahbkiii@gmail.com>
2023-06-13 16:49:06 +0900

    修正

--- resources/js/views/register/User.vue
+++ resources/js/views/register/User.vue
@@ -554,6 +554,7 @@
                         <p v-show="v$.$error" class="text-danger">入力に誤りがあります</p>
                         <p class="text-danger">あああ</p>
                         <p class="text-danger">{{ errMsg }}</p>
+                        <p class="text-danger">{{ message.data.message }}</p>
                         <button type="submit" class="btn  btn--accent">確認画面</button>
                     </div>
                 </form>
