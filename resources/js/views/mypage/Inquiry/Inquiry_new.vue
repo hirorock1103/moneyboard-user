a777b4ffb845cb009122483da15d7177f9e9b910
AMB\myama <m.yamamoto@ambi-tious.com>
2024-04-19 17:45:13 +0900

    PPF-278 使用料の請求について（お問い合わせ）の制御

--- resources/js/views/mypage/Inquiry/Inquiry_new.vue
+++ resources/js/views/mypage/Inquiry/Inquiry_new.vue
@@ -76,9 +76,9 @@
                                                         <option value="4">
                                                             その他
                                                         </option>
-                                                        <option value="5">
+                                                        <!-- <option value="5">
                                                             使用料の請求について
-                                                        </option>
+                                                        </option> -->
                                                     </select>
                                                     <div
                                                         class="form-text  text-danger text-center [ margin-bottom--24   ]  "
