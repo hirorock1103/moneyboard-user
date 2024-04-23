d1cdbc57ff194f84202a372c8aa7913b7d96c38d
Hatano-AB <hatano@ambi-tious.com>
2024-04-23 17:20:04 +0900

    PPF-293 お問い合わせ種別の追加

--- resources/js/views/mypage/Inquiry/Inquiry_add.vue
+++ resources/js/views/mypage/Inquiry/Inquiry_add.vue
@@ -62,7 +62,8 @@
                                                 <td v-if="temps.type == 2" class="[ display-table-row  display-table-cell-large ] ">使い方について</td>
                                                 <td v-if="temps.type == 3" class="[ display-table-row  display-table-cell-large ] ">ご意見・ご要望</td>
                                                 <td v-if="temps.type == 4" class="[ display-table-row  display-table-cell-large ] ">その他</td>
-                                                <td v-if="temps.type == 5" class="[ display-table-row  display-table-cell-large ] ">使用料の請求について</td>
+                                                <td v-if="temps.type == 5" class="[ display-table-row  display-table-cell-large ] ">利用料のご請求について</td>
+                                                <td v-if="temps.type == 6" class="[ display-table-row  display-table-cell-large ] ">運営からのご連絡</td>
                                             </tr>
                                             <tr>
                                                 <th class="vertical-top [ padding-top--16 padding-top-large--24 ][ display-table-row  display-table-cell-large ]">
