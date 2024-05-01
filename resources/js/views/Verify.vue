962cfd6304a9ad651f475f49b8e43641ddb6c502
takenawa <>
2024-05-01 13:00:46 +0900

    調整
    調整

--- resources/js/views/Verify.vue
+++ resources/js/views/Verify.vue
@@ -1,19 +1,30 @@
 <template>
     <main>
-        <section class="[ padding-top--24 padding-top-large--48 ] margin-bottom-large--48">
+        <section
+            class="[ padding-top--24 padding-top-large--48 ] margin-bottom-large--48"
+        >
             <div class="container">
-                <h2 v-show="!verifyErrors" class="text-center  heading-primary">メールアドレスの確認</h2>
-                <p v-show="!verifyErrors" class="text-center">少々お待ちください…</p>
-                <loading v-model:active="loadingStatus"
-                        :can-cancel="false"
-                        :is-full-page="false"
-                        :color="'#2FBCED'"
-                        :height="90"
-                        :width="100" />
+                <h2 v-show="!verifyErrors" class="text-center heading-primary">
+                    メールアドレスの確認
+                </h2>
+                <p v-show="!verifyErrors" class="text-center">
+                    {{ api_message }}
+                </p>
+                <loading
+                    v-model:active="loadingStatus"
+                    :can-cancel="false"
+                    :is-full-page="false"
+                    :color="'#2FBCED'"
+                    :height="90"
+                    :width="100"
+                />
                 <div
-                    class="form-text  text-danger  margin-bottom--24"
-                    v-if="verifyErrors">
-                    <span v-for="msg in verifyErrors" :key="msg">{{ msg }}</span>
+                    class="form-text text-danger margin-bottom--24"
+                    v-if="verifyErrors"
+                >
+                    <span v-for="msg in verifyErrors" :key="msg">{{
+                        msg
+                    }}</span>
                 </div>
             </div>
         </section>
@@ -21,41 +32,53 @@
 </template>
 
 <script>
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
-import { mapGetters, mapState, mapActions } from 'vuex';
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+import { mapGetters, mapState, mapActions } from "vuex";
 
 export default {
-    name: 'verify',
+    name: "verify",
     components: {
-        Loading
+        Loading,
     },
-    data() {},
+    data() {
+        return {
+            api_message: "少々お待ちください…",
+        };
+    },
     mounted() {
         this.sendVerifyRequest(this.$route.query.token).then(() => {
             this.resetTemps();
             this.updateTemps(this.$route.query.email);
+            console.log(this.apiStatus);
             if (this.apiStatus) {
-                this.$router.push({ name:'register-user'});
+                this.$router.push({ name: "register-user" });
+            } else {
+                this.api_message =
+                    "すでに登録済のメールアドレスの可能性がございます";
             }
         });
     },
     computed: {
-        ...mapGetters('auth', ['user']),
+        ...mapGetters("auth", ["user"]),
         ...mapState({
-            apiStatus: state => state.auth.apiStatus,
-            verifyErrors: state => state.auth.verifyErrorMessages,
-            loadingStatus: state => state.auth.loadingStatus,
-        })
+            apiStatus: (state) => state.auth.apiStatus,
+            verifyErrors: (state) => state.auth.verifyErrorMessages,
+            loadingStatus: (state) => state.auth.loadingStatus,
+        }),
     },
     methods: {
-        ...mapActions('auth', ['sendVerifyRequest', 'updateTemps', 'resetTemps']),
-        clearError () {
-            this.$store.commit('auth/setVerifyErrorMessages', null)
-        }
+        ...mapActions("auth", [
+            "sendVerifyRequest",
+            "updateTemps",
+            "resetTemps",
+        ]),
+        clearError() {
+            this.$store.commit("auth/setVerifyErrorMessages", null);
+        },
     },
-    created () {
-        this.clearError()
-    }
+    created() {
+        this.clearError();
+    },
 };
 </script>
