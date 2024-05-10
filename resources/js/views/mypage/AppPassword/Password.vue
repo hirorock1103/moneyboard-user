eeba620564999d77badca73e623ae7e645f200c3
AMB\myama <m.yamamoto@ambi-tious.com>
2024-05-10 18:19:25 +0900

    PPF-322 マイページのパスワード入力と表示箇所の全てに目のマークを追加

--- resources/js/views/mypage/AppPassword/Password.vue
+++ resources/js/views/mypage/AppPassword/Password.vue
@@ -59,11 +59,16 @@
                                                 </th>
                                                 <td class="[ display-table-row  display-table-cell-large ] ">
                                                     <input
-                                                    type="password"
+                                                    :type="confirmationPassword ? 'text' : 'password'"
                                                     class="form-input  margin-top--8"
                                                     v-model="item.password_confirm"
                                                         @input="v$.item.password_confirm.$touch"
                                                         v-bind:class="[ v$.item.password_confirm.$error ? 'form-error' : null ]"/>
+                                                    <span class="input-icon">
+                                                        <span :class="confirmationPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"
+                                                        @click="confirmationPassword = !confirmationPassword"
+                                                        class="password-icon"></span>
+                                                    </span>
                                                     <div
                                                         class="form-text  text-danger  text-center"
                                                         v-if="v$.item.password_confirm.$error">
@@ -113,6 +118,7 @@ export default {
             item: {},
             message: "",
             showPassword: false,
+            confirmationPassword: false,
         };
     },
     watch: {
